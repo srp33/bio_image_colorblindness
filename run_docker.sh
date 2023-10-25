3f2439da09e0c86b326c10556e282af4974cdc0f
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2023-10-25 11:07:51 -0600

    Saved results for iteration 3

--- run_docker.sh
+++ run_docker.sh
@@ -7,7 +7,6 @@ container_name="bio_image_colorblindness"
 docker build -t ${image_name}:version$(cat VERSION) \
              -t ${image_name}:latest \
              .
-
 mkdir -p TempResults
 
 #  -i -t \
