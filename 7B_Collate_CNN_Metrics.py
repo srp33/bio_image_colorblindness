63efc14d6ced8bb41b89dbc6e5454e61b666efc1
srp33 <goldfish1434@yahoo.com>
2023-11-08 06:26:00 -0700

    Summarized results

--- 7B_Collate_CNN_Metrics.py
+++ 7B_Collate_CNN_Metrics.py
@@ -4,13 +4,17 @@ import sys
 out_file_path = "Cross_Validation_Results_CNN.tsv"
 
 with open(out_file_path, "w") as out_file:
-    out_file.write("iteration\tfold\talgorithm\tauroc\n")
+    out_file.write("iteration\tfold\timage_type\talgorithm\tauroc\n")
 
     for metrics_file_path in sorted(glob(f"CNN_Metrics/model_*/iteration_*/fold_*/metrics.tsv")):
         parts = metrics_file_path.split("/")
-        model = parts[1].split("_")[1]
         iteration = parts[2].split("_")[1]
         fold = parts[3].split("_")[1]
+        model = parts[1].split("_")[1]
+
+        image_type = "original"
+        if len(parts[1].split("_")) > 2:
+            image_type = parts[1].split("_")[2]
 
         auroc = None
         with open(metrics_file_path) as metrics_file:
@@ -18,4 +22,4 @@ with open(out_file_path, "w") as out_file:
                 if line.startswith("auc"):
                     auroc = float(line.rstrip("\n").split("\t")[1])
 
-        out_file.write(f"{iteration}\t{fold}\t{model}\t{auroc}\n")
+        out_file.write(f"{iteration}\t{fold}\t{image_type}\t{model}\t{auroc}\n")
