112bede7f6292a311e7fbc3ba7e9f95fb785ee27
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2023-11-13 15:50:39 -0700

    Saved misclassifications file

--- 12_Plot_CNN_Testing.R
+++ 12_Plot_CNN_Testing.R
@@ -12,11 +12,11 @@ ggplot(data = data, aes(x = label, y = probability_unfriendly, col = label)) +
 
 misclassification_summary1 = arrange(data, probability_unfriendly) %>%
   filter(label == "unfriendly") %>%
-  head(n = 10)
+  filter(probability_unfriendly < 0.5)
 
 misclassification_summary2 = arrange(data, desc(probability_unfriendly)) %>%
   filter(label == "friendly") %>%
-  head(n = 10)
+  filter(probability_unfriendly > 0.5)
 
 bind_rows(misclassification_summary1, misclassification_summary2) %>%
   write_xlsx("CNN_Top_Misclassifications.xlsx")
