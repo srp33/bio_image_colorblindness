ab9ad94cd99bcd3a4654c77c14f7ab0f937a50a3
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2023-11-17 09:42:42 -0700

    Tweaked values for consistency

--- 12_Plot_CNN_Testing.R
+++ 12_Plot_CNN_Testing.R
@@ -8,7 +8,7 @@ save_fig = function(file_name, width = 6.5, height=5) {
 }
 
 data = read_tsv("CNN_Metrics_final/predictions.tsv") %>%
-  mutate(label = ifelse(label == "friendly", "Friendly", "Unfriendly"))
+  mutate(label = ifelse(label == "friendly", "Definitely okay", "Definitely problematic"))
 
 ggplot(data = data, aes(x = label, y = probability_unfriendly, col = label)) +
   geom_boxplot(outlier.shape = NA) +
@@ -16,7 +16,7 @@ ggplot(data = data, aes(x = label, y = probability_unfriendly, col = label)) +
   theme_bw(base_size = 18) +
   guides(color = FALSE) +
   xlab("") +
-  ylab("Confidence score (Unfriendly)") +
+  ylab("Confidence score (Definitely problematic)") +
   scale_color_manual(values = c("#018571", "#a6611a"))
 
 save_fig("CNN_Testing_predictions")
@@ -29,24 +29,29 @@ ggroc(r, color = "darkred") +
   geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color = "grey", linetype = "dashed") +
   scale_y_continuous(expand = c(0, 0)) +
   scale_x_reverse(expand = c(0, 0)) +
-  theme_bw(base_size = 18) +
-  annotate("text", x = 0.33, y = 0.05, label = legend_text)
+  theme_bw(base_size = 18)
 
 save_fig("CNN_Testing_ROC")
 
 misclassification_summary1 = arrange(data, probability_unfriendly) %>%
-  filter(label == "Unfriendly") %>%
+  filter(label == "Definitely problematic") %>%
   filter(probability_unfriendly < 0.5) %>%
-  mutate(predicted_label = "Friendly") %>%
+  mutate(predicted_label = "Definitely okay") %>%
   select(image_file_path, label, predicted_label, probability_unfriendly)
 
 misclassification_summary2 = arrange(data, desc(probability_unfriendly)) %>%
-  filter(label == "Friendly") %>%
+  filter(label == "Definitely okay") %>%
   filter(probability_unfriendly > 0.5) %>%
-  mutate(predicted_label = "Unfriendly") %>%
+  mutate(predicted_label = "Definitely problematic") %>%
   select(image_file_path, label, predicted_label, probability_unfriendly)
 
 bind_rows(misclassification_summary1, misclassification_summary2) %>%
+  dplyr::rename(probability_problematic = probability_unfriendly) %>%
   write_xlsx("CNN_Misclassifications_Testing.xlsx")
 
-#data = read_tsv("CNN_Metrics_final/epoch_metrics.tsv")
+#######################################################
+
+read_xlsx("CNN_Misclassifications_Testing_Annotated.xlsx") %>%
+  pull(Conclusion) %>%
+  table() %>%
+  print()
