820b58a785fe1c0f00af45c80c3e06cc5ee0e877
srp33 <goldfish1434@yahoo.com>
2023-11-24 08:23:47 -0700

    Added the 13th step

--- exec_inside_docker.sh
+++ exec_inside_docker.sh
@@ -82,3 +82,4 @@ Rscript 9_Analyze_Image_Metrics_Testing.R
 python3 10_Classify_Using_Metrics_Testing.py
 python3 11_Classify_Using_CNN_Testing.py
 Rscript 12_Plot_CNN_Testing.R
+python3 13_Export_Model_JS.py
